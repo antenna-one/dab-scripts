5d8ae94e7736431da2f88d8422d4a53c8309bd04
antenna.one <104686113+antenna-one@users.noreply.github.com>
2022-06-04 12:39:01 +0100

    Update cleanup.sh

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -2,10 +2,8 @@
 echo -e "$GREEN *************************************************************** $NORMAL"
 echo -e "$GREEN *          Removing Applications That Are Not Needed          * $NORMAL"
 echo -e "$GREEN *************************************************************** $NORMAL"
-sudo apt-get remove --auto-remove -y thunderbird transmission-gtk gnome-mines \
-pidgin sgt-puzzles simple-scan atril ristretto xfburn exfalso \
-xfce4-screenshooter parole gnome-sudoku gimp goldendict \
-info gigolo mate-calc atril xsane aspell hunspell myspell xfce4-dict
-
+sudo apt-get remove --auto-remove -y thunderbird transmission-gtk gnome-mines pidgin sgt-puzzles
+sudo apt-get remove --auto-remove -y simple-scan atril ristretto xfburn exfalso xfce4-screenshooter
+sudo apt-get remove --auto-remove -y parole gnome-sudoku gimp goldendict xsane info gigolo
+sudo apt-get remove --auto-remove -y mate-calc atril xsane aspell hunspell myspell xfce4-dict
 sudo apt-get remove --auto-remove -y libreoffice*
-
